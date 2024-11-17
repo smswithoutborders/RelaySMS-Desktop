4ce33e3fa6c03a0000dc295e64975727be05b1a8
Vanessa Christopher <vanessaigwe1@gmail.com>
2024-11-17 20:45:02 +0100

    refactor: update signup page

--- src/Pages/SignupPage.js
+++ src/Pages/SignupPage.js
@@ -1,159 +1,405 @@
-import { CheckBox } from "@mui/icons-material";
+import React, { useState } from "react";
 import {
   Box,
   Button,
-  Grid,
+  Grid2 as Grid,
   TextField,
   Typography,
   Link,
-  FormControlLabel,
+  IconButton,
+  Snackbar,
+  Alert as MuiAlert,
+  Checkbox,
 } from "@mui/material";
-import { MuiTelInput } from "mui-tel-input";
-import React from "react";
+import { MuiTelInput, matchIsValidTel } from "mui-tel-input";
 import { Link as RouterLink } from "react-router-dom";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import { OTPDialog } from "../Components";
 
 function SignupPage() {
+  const [phone, setPhone] = useState("");
+  const [phoneInfo, setPhoneInfo] = useState({});
+  const [passwordData, setPasswordData] = useState({
+    password: "",
+    confirmPassword: "",
+    passwordError: false,
+    confirmPasswordError: false,
+  });
+  const [showPassword, setShowPassword] = useState({
+    password: false,
+    confirmPassword: false,
+  });
+  const [phoneError, setPhoneError] = useState(false);
+  const [phoneErrorMessage, setPhoneErrorMessage] = useState("");
+  const [alert, setAlert] = useState({
+    open: false,
+    type: "success",
+    message: "",
+  });
+  const [otpDialogOpen, setOtpDialogOpen] = useState(false);
+  const [agreedToTerms, setAgreedToTerms] = useState(false);
+
+  const handlePhoneChange = (value, info) => {
+    info.countryCode = info.countryCode
+      ? info.countryCode
+      : phoneInfo.countryCode;
+    setPhone(value);
+    setPhoneInfo(info);
+    setPhoneError(false);
+    setPhoneErrorMessage("");
+  };
+
+  const handlePasswordFieldChange = (event) => {
+    const { name, value } = event.target;
+    setPasswordData((prevState) => ({
+      ...prevState,
+      [name]: value,
+      passwordError: name === "password" ? false : prevState.passwordError,
+      confirmPasswordError:
+        name === "confirmPassword" ? false : prevState.confirmPasswordError,
+    }));
+  };
+
+  const handleTermsChange = (event) => {
+    setAgreedToTerms(event.target.checked);
+  };
+
+  const validatePhoneNumber = () => {
+    if (!phone || !phoneInfo.nationalNumber) {
+      setPhoneErrorMessage("Phone number is required");
+      return false;
+    }
+
+    if (!matchIsValidTel(phone)) {
+      if (!phoneInfo.countryCode) {
+        setPhoneErrorMessage(
+          "Please select a country and enter your phone number."
+        );
+      } else {
+        const regionNames = new Intl.DisplayNames(["en"], { type: "region" });
+        const countryName = regionNames.of(phoneInfo.countryCode);
+        setPhoneErrorMessage(
+          `Please enter a valid phone number for ${countryName}.`
+        );
+      }
+      return false;
+    }
+
+    return true;
+  };
+
+  const validatePassword = () => {
+    const { password, confirmPassword } = passwordData;
+
+    if (!password) {
+      setPasswordData((prevState) => ({
+        ...prevState,
+        passwordError: true,
+      }));
+      return false;
+    }
+
+    if (password !== confirmPassword) {
+      setPasswordData((prevState) => ({
+        ...prevState,
+        confirmPasswordError: true,
+      }));
+      return false;
+    }
+
+    return true;
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    setPhoneError(false);
+
+    const isPhoneValid = validatePhoneNumber();
+    const isPasswordValid = validatePassword();
+
+    if (!isPhoneValid || !isPasswordValid || !agreedToTerms) {
+      if (!isPhoneValid) setPhoneError(true);
+      return;
+    }
+
+    console.log(
+      "Phone:",
+      phone,
+      "Password:",
+      passwordData.password,
+      "Country Code",
+      phoneInfo.countryCode
+    );
+
+    setTimeout(() => {
+      const success = true;
+      if (success) {
+        setAlert({
+          open: true,
+          type: "success",
+          message: "Login successful! Please enter the OTP.",
+        });
+        setOtpDialogOpen(true);
+      } else {
+        setAlert({
+          open: true,
+          type: "error",
+          message: "Invalid credentials, please try again.",
+        });
+      }
+    }, 1000);
+  };
+
+  const togglePasswordVisibility = (field) => {
+    setShowPassword((prevState) => ({
+      ...prevState,
+      [field]: !prevState[field],
+    }));
+  };
+
+  const handleOtpSubmit = (otp) => {
+    console.log("OTP Submitted:", otp);
+
+    setTimeout(() => {
+      const otpSuccess = otp === "123456";
+      if (otpSuccess) {
+        setAlert({
+          open: true,
+          type: "success",
+          message: "OTP verified successfully!",
+        });
+        setOtpDialogOpen(false);
+      } else {
+        setAlert({
+          open: true,
+          type: "error",
+          message: "Invalid OTP, please try again.",
+        });
+        setOtpDialogOpen(true);
+      }
+    }, 1000);
+  };
+
   return (
-    <Box
-      sx={{
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <Grid
+      container
+      height="100vh"
+      justifyContent="center"
+      alignItems="center"
+      columnSpacing={4}
     >
       <Grid
-        container
-        columnSpacing={4}
+        size={8}
+        display="flex"
+        height="100%"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        textAlign="center"
         sx={{
-          justifyContent: "center",
-          alignItems: "center",
+          py: 5,
+          px: { xs: 5, md: 18 },
+          overflowY: "auto",
         }}
       >
-        <Grid item md={8} xs={8}>
-          <Box component="form" sx={{ px: 15 }}>
-            <Typography
-              variant="h3"
-              sx={{ fontWeight: 600, py: 2, textAlign: "center" }}
-            >
-              Sign Up
-            </Typography>
-            <Typography variant="h6" sx={{ py: 3, textAlign: "center" }}>
-              Already have an account?{" "}
-              <Link
-                component={RouterLink}
-                to="/login"
-                sx={{
-                  color: "background.more",
-                  textDecoration: "none",
-                  fontWeight: "bold",
-                  "&:hover": { textDecoration: "underline" },
-                }}
-              >
-                Login
-              </Link>
-            </Typography>
-            <MuiTelInput
-              fullWidth
-              variant="standard"
-              defaultCountry="CM"
-              sx={{ py: 2 }}
-            />
-            <TextField
-              fullWidth
-              label="Password"
-              variant="standard"
-              sx={{ mt: 8 }}
-            />
-            <TextField
-              fullWidth
-              label="Repeat Password"
-              variant="standard"
-              sx={{ mt: 8 }}
-            />
+        <Typography variant="h3" sx={{ fontWeight: 600, mb: 8 }}>
+          Sign Up
+        </Typography>
+        <Typography variant="h6" sx={{ py: 5 }}>
+          Have an account?{" "}
+          <Link
+            component={RouterLink}
+            to="/login"
+            sx={{
+              color: "background.more",
+              textDecoration: "none",
+              fontWeight: "bold",
+              "&:hover": { textDecoration: "underline" },
+            }}
+          >
+            Login
+          </Link>
+        </Typography>
 
-            <FormControlLabel
-              control={<CheckBox name="acceptPolicy" />}
-              label={<span>Accept Privacy Policy</span>}
-              sx={{ my: 4 }}
-            />
-
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                mt: 5,
-              }}
-            >
-              <Button
-                variant="contained"
-                size="large"
-                sx={{
-                  bgcolor: "background.more",
-                  color: "white",
-                  borderRadius: 7,
-                  px: 15,
-                  py: 2,
-                  textTransform: "none",
-                }}
-              >
-                Sign Up
-              </Button>
-            </Box>
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                mt: 10,
-              }}
-            >
-              <Typography variant="h6" sx={{ textDecoration: "underline" }}>
-                <Link
-                  component={RouterLink}
-                  to="/bridge-auth"
-                  sx={{
-                    textDecoration: "none",
-                    "&:hover": { textDecoration: "underline" },
-                  }}
-                >
-                  Authenticate Offline
-                </Link>
-              </Typography>
-            </Box>
-          </Box>
-        </Grid>
-
-        <Grid
-          item
-          xs={4}
-          md={4}
-          sx={{
-            bgcolor: "background.paper",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100vh",
-            px: 8,
-          }}
+        <Snackbar
+          open={alert.open}
+          autoHideDuration={4000}
+          onClose={() => setAlert({ ...alert, open: false })}
+          anchorOrigin={{ vertical: "top", horizontal: "center" }}
         >
-          <Box>
-            <Box
-              component="img"
-              src="https://img.freepik.com/premium-vector/qr-code-isolated-white-background-universal-product-scan-code-doodle-style-vector-icon-design-element-hand-drawn_186802-2493.jpg?ga=GA1.2.2003631022.1724151815&semt=ais_hybrid"
-              alt="qr code"
+          <MuiAlert
+            severity={alert.type}
+            sx={{ width: "100%", maxWidth: 600, margin: "0 auto" }}
+          >
+            {alert.message}
+          </MuiAlert>
+        </Snackbar>
+
+        <MuiTelInput
+          fullWidth
+          variant="standard"
+          value={phone}
+          onChange={handlePhoneChange}
+          defaultCountry="CM"
+          forceCallingCode
+          focusOnSelectCountry
+          required
+          error={phoneError}
+          helperText={phoneError ? phoneErrorMessage : ""}
+          sx={{
+            py: 2,
+            "& .MuiInput-root": {
+              borderRadius: 4,
+              backgroundColor: "background.default",
+              boxShadow: "0 2px 5px rgba(0,0,0,0.1)",
+            },
+          }}
+        />
+
+        <TextField
+          fullWidth
+          label="Password"
+          variant="standard"
+          name="password"
+          value={passwordData.password}
+          onChange={handlePasswordFieldChange}
+          type={showPassword.password ? "text" : "password"}
+          required
+          error={passwordData.passwordError}
+          helperText={passwordData.passwordError ? "Password is required" : ""}
+          sx={{ mt: 8 }}
+          slotProps={{
+            input: {
+              endAdornment: (
+                <IconButton
+                  onClick={() => togglePasswordVisibility("password")}
+                  edge="end"
+                >
+                  {showPassword.password ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              ),
+            },
+          }}
+        />
+
+        <TextField
+          fullWidth
+          label="Confirm Password"
+          variant="standard"
+          name="confirmPassword"
+          value={passwordData.confirmPassword}
+          onChange={handlePasswordFieldChange}
+          type={showPassword.confirmPassword ? "text" : "password"}
+          required
+          error={passwordData.confirmPasswordError}
+          helperText={
+            passwordData.confirmPasswordError ? "Passwords must match." : ""
+          }
+          sx={{ mt: 8 }}
+          slotProps={{
+            input: {
+              endAdornment: (
+                <IconButton
+                  onClick={() => togglePasswordVisibility("confirmPassword")}
+                  edge="end"
+                >
+                  {showPassword.confirmPassword ? (
+                    <VisibilityOff />
+                  ) : (
+                    <Visibility />
+                  )}
+                </IconButton>
+              ),
+            },
+          }}
+        />
+
+        <Box display="flex" alignItems="center" sx={{ mt: 4 }}>
+          <Checkbox
+            checked={agreedToTerms}
+            onChange={handleTermsChange}
+            color="primary"
+          />
+          <Typography variant="body2">
+            I agree to the{" "}
+            <Link
+              to="/terms"
+              component={RouterLink}
               sx={{
-                width: "100%",
-                height: "100%",
-                objectFit: "cover",
-                borderRadius: 7,
+                textDecoration: "none",
+                fontWeight: "bold",
+                "&:hover": { textDecoration: "underline" },
               }}
-            />
-          </Box>
-        </Grid>
+            >
+              Terms and Conditions
+            </Link>
+          </Typography>
+        </Box>
+
+        <Button
+          variant="contained"
+          size="large"
+          sx={{
+            mt: 5,
+            borderRadius: 7,
+            width: "50%",
+            bgcolor: "background.more",
+            color: "white",
+            "&:hover": {
+              bgcolor: "primary.main",
+              color: "black",
+            },
+          }}
+          onClick={handleSubmit}
+          disabled={!agreedToTerms}
+        >
+          Sign Up
+        </Button>
+
+        <Box sx={{ mt: 3 }}>
+          <Typography variant="h6" sx={{ mt: 10 }}>
+            <Link component={RouterLink} to="/bridge-auth" underline="always">
+              Authenticate Offline
+            </Link>
+          </Typography>
+        </Box>
       </Grid>
-    </Box>
+      <Grid
+        size={4}
+        height="100%"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        sx={{
+          bgcolor: "background.paper",
+          p: 2,
+          overflowY: "auto",
+        }}
+      >
+        <img
+          src="https://img.freepik.com/premium-vector/qr-code-isolated-white-background-universal-product-scan-code-doodle-style-vector-icon-design-element-hand-drawn_186802-2493.jpg?ga=GA1.2.2006603533.1697742214&semt=sph"
+          alt="QR Code"
+          style={{ width: "100%", height: "auto", maxWidth: "300px" }}
+        />
+      </Grid>
+
+      <OTPDialog
+        open={otpDialogOpen}
+        onClose={() => setOtpDialogOpen(false)}
+        onSubmit={handleOtpSubmit}
+        onResend={() => {
+          console.log("Resend OTP requested");
+          setAlert({
+            message: "A new OTP has been sent to your mobile number.",
+            severity: "info",
+          });
+        }}
+        counterTimestamp={Math.floor(Date.now() / 1000) + 30}
+        alert={alert}
+      />
+    </Grid>
   );
 }
 
