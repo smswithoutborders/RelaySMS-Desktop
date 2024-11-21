c6a38d0265c9265494cb4a04cd4be98e3edd6ff5
Vanessa Christopher <vanessaigwe1@gmail.com>
2024-11-21 12:47:49 +0100

    feat: add app toggle to side nav

--- src/Components/NavigationPanel.jsx
+++ src/Components/NavigationPanel.jsx
@@ -1,18 +1,17 @@
 import React, { useState, useEffect } from "react";
 import {
+  Box,
   List,
-  ListItem,
+  ListItemButton,
   ListItemIcon,
   ListItemText,
-  Drawer,
   Divider,
   IconButton,
   Tooltip,
   Skeleton,
-  ListItemButton,
-  Box,
 } from "@mui/material";
 import { ChevronLeft, ChevronRight } from "@mui/icons-material";
+import { Link } from "react-router-dom";
 
 function NavigationPanel({ items = [], app }) {
   const [isExpanded, setIsExpanded] = useState(true);
@@ -31,118 +30,140 @@ function NavigationPanel({ items = [], app }) {
   };
 
   return (
-    <Drawer
-      variant="permanent"
+    <Box
       sx={{
-        width: isExpanded ? 240 : 72,
-        transition: "width 0.5s ease",
-        flexShrink: 0,
-        "& .MuiDrawer-paper": {
-          width: isExpanded ? 240 : 72,
-          transition: "width 0.5s ease",
-          overflowX: "hidden",
-        },
+        display: "flex",
+        flexDirection: "row",
+        height: "100vh",
+        overflow: "hidden",
+        bgcolor: "background.paper",
       }}
     >
-      <List>
-        <ListItemButton
-          onClick={handleToggle}
-          sx={{
-            display: "flex",
-            justifyContent: isExpanded ? "flex-end" : "center",
-            transition: "justify-content 0.5s ease",
-          }}
-        >
-          <IconButton>
-            {isExpanded ? <ChevronLeft /> : <ChevronRight />}
-          </IconButton>
-        </ListItemButton>
+      <Box
+        sx={{
+          width: 72,
+          mx: "auto",
+        
+          flexDirection: "column",
+          mt: 2,
+          padding: 1,
+          zIndex: 1,
+        }}
+      >
+        <Tooltip title="Home" placement="right">
+          <Link to="/">
+          <ListItemButton>
+            <ListItemIcon>
+              <Box
+                component="img"
+                src="logo.png"
+                alt="Relay"
+                sx={{ width: "33px", bgcolor: "#eaeaea", p: 0.5, borderRadius: 1 }}
+              />
+            </ListItemIcon>
+          </ListItemButton>
+          </Link>
+        </Tooltip>
+        <Tooltip title="Settings" placement="right">
+        <Link to="/deku">
+          <ListItemButton>
+            <ListItemIcon>
+              <Box
+                component="img"
+                src="Deku.png"
+                alt="Deku"
+                sx={{ width: "33px", bgcolor: "#eaeaea", p: 0.5, borderRadius: 1 }}
+              />
+            </ListItemIcon>
+          </ListItemButton>
+          </Link>
+        </Tooltip>
+      </Box>
+      <Divider orientation="vertical" />
 
-        <Divider />
+      <Box
+        variant="permanent"
+        sx={{
+          flexShrink: 0,
+          width: isExpanded ? 240 : 72,
 
-        {items.length > 0
-          ? items.map((item, index) => (
-              <Tooltip
-                title={!isExpanded && item.text ? item.text : ""}
-                placement="right"
-                key={index}
-              >
+          transition: "width 0.5s ease",
+          "& .MuiDrawer-paper": {
+            width: isExpanded ? 240 : 72,
+            transition: "width 0.5s ease",
+            overflowX: "hidden",
+          },
+        }}
+      >
+        <List>
+          <ListItemButton
+            onClick={handleToggle}
+            sx={{
+              display: "flex",
+              justifyContent: isExpanded ? "flex-end" : "center",
+              transition: "justify-content 0.5s ease",
+            }}
+          >
+            <IconButton>
+              {isExpanded ? <ChevronLeft /> : <ChevronRight />}
+            </IconButton>
+          </ListItemButton>
+
+          <Divider />
+
+          {items.length > 0
+            ? items.map((item, index) => (
+                <Tooltip
+                  title={!isExpanded && item.text ? item.text : ""}
+                  placement="right"
+                  key={index}
+                >
+                  <ListItemButton
+                    onClick={item.action}
+                    sx={{
+                      padding: "10px 20px",
+                      cursor: "pointer",
+                    }}
+                  >
+                    {item.icon ? (
+                      <ListItemIcon>{item.icon}</ListItemIcon>
+                    ) : (
+                      <Skeleton variant="circular" width={24} height={24} />
+                    )}
+                    <ListItemText
+                      primary={item.text}
+                      sx={{
+                        opacity: isExpanded ? 1 : 0,
+                        transition: "opacity 0.5s ease, visibility 0.5s ease",
+                        whiteSpace: "nowrap",
+                        textOverflow: "ellipsis",
+                      }}
+                    />
+                  </ListItemButton>
+                </Tooltip>
+              ))
+            : Array.from({ length: 3 }).map((_, index) => (
                 <ListItemButton
-                  onClick={item.action}
+                  key={index}
                   sx={{
                     padding: "10px 20px",
                     cursor: "pointer",
                   }}
                 >
-                  {item.icon ? (
-                    <ListItemIcon>{item.icon}</ListItemIcon>
-                  ) : (
-                    <Skeleton variant="circular" width={24} height={24} />
-                  )}
+                  <Skeleton variant="circular" width={24} height={24} />
                   <ListItemText
-                    primary={item.text}
+                    primary=""
                     sx={{
                       opacity: isExpanded ? 1 : 0,
                       transition: "opacity 0.5s ease, visibility 0.5s ease",
-                      whiteSpace: "nowrap",
-                      textOverflow: "ellipsis",
                     }}
                   />
                 </ListItemButton>
-              </Tooltip>
-            ))
-          : Array.from({ length: 3 }).map((_, index) => (
-              <ListItem
-                key={index}
-                sx={{
-                  padding: "10px 20px",
-                  cursor: "pointer",
-                }}
-              >
-                <Skeleton variant="circular" width={24} height={24} />
-                <ListItemText
-                  primary=""
-                  sx={{
-                    opacity: isExpanded ? 1 : 0,
-                    transition: "opacity 0.5s ease, visibility 0.5s ease",
-                  }}
-                />
-              </ListItem>
-            ))}
-      </List>
-
-      {app && (
-        <List sx={{ mt: "auto" }}>
-          <ListItemButton
-            onClick={app.action}
-            size="small"
-            sx={{
-              cursor: "pointer",
-              bgcolor: "background.primary",
-              mb: 2,
-            }}
-          >
-            <ListItemIcon>
-              <Box
-                component="img"
-                src={app.icon || ""}
-                style={{ width: "35px" }}
-                alt={app.text || ""}
-              />
-            </ListItemIcon>
-            <ListItemText
-              primary={app.text || ""}
-              sx={{
-                opacity: isExpanded ? 1 : 0,
-                transition: "opacity 0.5s ease, visibility 0.5s ease",
-                whiteSpace: "nowrap",
-                textOverflow: "ellipsis",
-              }}
-            />
-          </ListItemButton>
+              ))}
         </List>
-      )}
-    </Drawer>
+      </Box>
+      <Divider orientation="vertical" />
+    </Box>
   );
 }
 
